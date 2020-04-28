275a96f4e3c30a2786de390db4d37e023c61e871
Cristobal Welch <cristobal.welch@me.com>
2020-04-28 13:41:08 -0400

    Ingredients Get and Post

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 from pymongo import MongoClient
 
 import parameters
-from validators import hamburger_creator
+from validators import hamburger_creator, hamburger_search_by_id, ingredient_creator
 
 app = Flask(__name__)
 
@@ -27,18 +27,17 @@ def hamburguesa_get():
     print(f"Burgers: {all_hamburgers}")
     return jsonify(all_hamburgers), 200
 
+# Creates new burger
 @app.route("/hamburguesa", methods=["POST"])
 def hamburguesa_post():
 
     data = request.get_json()
-    print(data)
     valid = hamburger_creator(data)
 
     if valid['status'] == 'valid input':
         data['id'] = parameters.current_burger_id
         parameters.current_burger_id += 1
         inserted_burger = hamburgers.insert_one(data)
-        print("Inserted Burger: ", inserted_burger)
         response = {'status': 'hamburguesa creada'}
         return jsonify(response), 201
     
@@ -46,11 +45,45 @@ def hamburguesa_post():
         response = {'status': 'input invalido'}
         return jsonify(response), 400
 
+# Searches burger with id: id
 @app.route("/hamburguesa/<string:id>")
 def hamburguesa_get_by_id(id):
-    hamburgers_list = list(hamburgers.find({"id": id}, {"_id": 0}))
-    print("Hamburgers: ", hamburgers_list)
-    return ''
+
+    valid = hamburger_search_by_id(id)
+    if valid['status'] == 'invalid id':
+        response = {'status': 'id invalido'}
+        return jsonify(response), 400
+    else:
+        hamburgers_list = list(hamburgers.find({"id": int(id)}, {"_id": 0}))
+        if len(hamburgers_list) == 0:
+            response = {'status': 'hamburguesa inexistente'}
+            return jsonify(response), 404
+        else:
+            response = hamburgers_list[0]
+            return jsonify(response), 200
+
+
+@app.route("/ingrediente", methods=["GET"])
+def ingredient_get():
+    all_ingredients = list(ingredients.find({}, {"_id": 0}))
+    return jsonify(all_ingredients), 200
+
+
+# Creates new ingredient
+@app.route("/ingrediente", methods=["POST"])
+def ingredient_post():
+
+    data = request.get_json()
+    valid = ingredient_creator(data)
+
+    if valid['status'] == 'valid input':
+        inserted_ingredient = ingredients.insert_one(data)
+        print("Inserted Ingredient: ", inserted_ingredient)
+        response = {"status": "ingrediente creado"}
+        return jsonify(response), 201
+    else:
+        response = {"status": "Input invalido"}
+        return jsonify(response), 400
 
 if __name__ == "__main__":
     app.run(debug=True)
--- validators.py
+++ validators.py
@@ -23,5 +23,28 @@ def hamburger_creator(data):
         return response
     
 def hamburger_search_by_id(id):
-    return ''
+    if not id.isdigit():
+        response = {'status': 'invalid id'}
+        return response
+    response = {'status': 'valid id'}
+    return response
+
+def ingredient_creator(data):
+
+    valid_ingredient = True
+    keys = ['id', 'nombre', 'descripcion']
+    data_keys = data.keys()
+
+    for key in keys:
+        if key not in data_keys:
+            valid_ingredient = False
     
+    if valid_ingredient:
+        if not (isinstance(data['id'], int) and isinstance(data['nombre'], str) and isinstance(data['descripcion'], str)):
+            valid_ingredient = False
+    
+    if valid_ingredient:
+        response = {'status': 'valid input'}
+    else:
+        response = {'status': 'invalid input'}
+    return response
